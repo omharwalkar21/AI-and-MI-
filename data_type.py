03c332ca2e49b95336253891f34874313466447a
Om21 <62588879+omharwalkar21@users.noreply.github.com>
2021-08-16 16:17:48 +0530

    Add files via upload

---
+++ data_type.py
@@ -0,0 +1,19 @@
+#list
+list = [10,20,30, "python",40,50,"java","C",60,100]
+print(list)
+print(list[2:5])
+list.reverse()
+print(list)
+list.append("OM")
+print(list)
+list.insert(5,200)
+print(list)
+list.clear()
+print(list)
+
+#tuple
+tuple = (10,10,40,40,80,10,"OM",88,"java","C")
+print(tuple)
+print(tuple.index(80))
+print(tuple.count(10))
+
