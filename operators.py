629048afc4ce3f2ff37084f408fa03f3018fe69e
Om21 <62588879+omharwalkar21@users.noreply.github.com>
2021-08-17 14:59:35 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,36 @@
+
+
+A=40
+B=50 
+print('A+B=', A+B)
+print('A-B=', A-B)
+print('A*B=', A*B)
+print('A**B=', A**B)
+print('A/B=', A/B)
+print('A//B=', A//B)
+
+
+###
+
+R=10
+S=20
+
+print('R>S', R>S)
+print('R<S',R<S)
+print('R==S', R==S)
+print('R>=S',R>=S)
+print('R!=S',R!=S)
+
+#######
+
+x= 'om'
+y= 'Om'
+print(x<y)
+
+var1= (10,20,30)
+var2= (10,20,30,40)
+print('var2>var1', var2>var1)
+
+var3=(10,20)
+var4=('ten, twenty')
+print('var3>var4', var3>var4)
