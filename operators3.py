c2d0f3cb61a8d99c5a3098567335f1ba8e5aaeb0
Om21 <62588879+omharwalkar21@users.noreply.github.com>
2021-08-20 13:47:56 +0530

    Add files via upload

---
+++ operators3.py
@@ -0,0 +1,31 @@
+#bianry add
+A=22
+B=23
+print(A & B)
+
+#binary or 
+A=22
+B=23
+print(A | B)
+
+#binary x -or 
+A=22
+B=23
+print(A^B)
+
+#ones compliement 
+A=22
+print(~A)
+
+A=-22
+print(~A)
+
+#left shift 
+A=10
+B=1
+print(A<<B)
+
+#right shift 
+A=20
+B=2
+print(A>>B)
